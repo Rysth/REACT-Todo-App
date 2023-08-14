759ea345c0f33798fb8df6ed49518c867e7f5415
Rysth <johnpalacios.t@gmail.com>
2023-08-14 12:21:48 -0500

    Add form component.

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -9,6 +9,20 @@ function ToDo() {
           <h1 className="app-title">Todos</h1>
           <p className="app-text">Items will persist in the browser local storage</p>
         </div>
+        <form action="#" className="app-form">
+          <div className="form-group">
+            <label htmlFor="task" className="form-label">
+              <input
+                className="form-input"
+                type="text"
+                name="task"
+                id="task"
+                placeholder="Add task..."
+              />
+              <i className="fa-solid fa-circle-plus form-icon" />
+            </label>
+          </div>
+        </form>
       </div>
     </div>
   );
