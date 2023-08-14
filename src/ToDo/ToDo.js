fd257a9743a173eac5cf0a17f9b05d7cd7ad74af
Rysth <johnpalacios.t@gmail.com>
2023-08-14 11:33:25 -0500

    Create app header.

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './ToDo.css';
 
 function ToDo() {
   return (
     <div className="app">
-      <header className="app-header">
-        <h1 className="app-title">Todos</h1>
-        <p className="app-text">Items will persist in the browser local storage</p>
-      </header>
+      <div className="container">
+        <div className="app-header">
+          <h1 className="app-title">Todos</h1>
+          <p className="app-text">Items will persist in the browser local storage</p>
+        </div>
+      </div>
     </div>
   );
 }
