2e1abd28717d35c5f7c10ce71be07fa7f9f9360d
Rysth <johnpalacios.t@gmail.com>
2023-08-14 11:27:14 -0500

    Add global styles.

---
+++ src/ToDo/ToDo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ToDo() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">Todos</h1>
+        <p className="app-text">Items will persist in the browser local storage</p>
+      </header>
+    </div>
+  );
+}
+
+export default ToDo;
